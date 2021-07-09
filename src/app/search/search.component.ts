a0a1aef3be47aaa1f27c50e9c557d338a3a84fe3
koltennay52 <60151320+koltennay52@users.noreply.github.com>
2021-07-09 08:48:22 -0600

    added reg ex validation for us zip codes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,23 @@
-import { Component, } from "@angular/core";
-
+import { Component, Input } from '@angular/core';
 
 @Component({
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.css']
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-    zipcode: string = '';
+  @Input() zipcode: string = '';
+  validSearch = false;
+  errorMessage = '';
 
+  zipChange(): void {
+    if (/^\d{5}(-\d{4})?$/.test(this.zipcode)) {
+      this.validSearch = true;
+    } else {
+        this.validSearch = false;
+    }
+  }
+
+  errorSearch(): void {
+    this.errorMessage = 'Enter a valid US zipcode to proceed.';
+  }
 }
